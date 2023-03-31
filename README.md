300653099e2ff131fb5b8c3ca11b7b9ef737228e
michmoosch <michael.moore01@sjsu.edu>
2023-03-31 11:06:25 -0700

    one and done

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # vite-react-template
+
+## git pull
+
+## cd project
+
+## npm i
---
+++ vite-template/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="hero">
+      <div className="hero-content">
+        <div className="card">
+          <h1>Vite React Typescript Tailwind DaisyUI Template</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
